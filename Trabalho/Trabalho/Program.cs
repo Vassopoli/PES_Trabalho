2655c511b85b0fccd96eb2edb51b5a6d1d52f5de
Ali Vassopoli <alisson_vassopoli@hotmail.com>
2017-06-02 13:05:58 -0300

    Tudo passado para o c# sem a transposição de linguagem

--- Trabalho/Trabalho/Program.cs
+++ Trabalho/Trabalho/Program.cs
@@ -111,8 +111,6 @@ namespace Trabalho
             corProduto[12] = "VERMELHO";
             corProduto[13] = "ROXO";
             corProduto[14] = "CINZA";
-
-            // Mexendo na funcao de cima, mas o ali mexeu ali em baixo. e eu n sei disso huehuebr ele n me dise
         }
         static void IncluirProduto(ref string[] cdProduto, ref string[] nmProduto, ref string[] cdFabricante, ref string[] corProduto)
         {
@@ -168,10 +166,658 @@ namespace Trabalho
 
                     Console.ReadKey();
                     break;
-
-                    // EU, VASSOPOLI, APAGUEI A ZOEIRA TODA AMEM.
                 }
             }
         }
+
+        static void AlterarProduto(cadeia &cdProduto[], cadeia &nmProduto[], cadeia &cdFabricante[], cadeia &corProduto[])
+        {
+            // Declarando variáveis
+            inteiro i, sn, sn2
+
+        cadeia codigo, nada, guardarCod, guardarNome, guardarCodFab, guardarCor, espaco
+
+
+        escreva("Insira o Código do produto que deseja alterar: ")
+
+        leia(codigo)
+
+        codigo = tx.preencher_a_esquerda('0', 3, codigo)
+
+        limpa()
+
+
+        para(i = 0; i < 999; i++)
+		{
+                se(cdProduto[i] == codigo)
+
+            {
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                    espaco = ""
+
+                para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+				{
+                        espaco = espaco + " "
+
+                }
+
+                    // Exibe
+                    escreva("O produto à seguir será alterado:\n")
+
+                escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i])
+
+                //variaveis que guardam o vetor inicial
+                    guardarCod = cdProduto[i]
+
+                guardarNome = nmProduto[i]
+
+                guardarCodFab = cdFabricante[i]
+
+                guardarCor = corProduto[i]
+
+                //Confirmaão do usuário
+                    escreva("\n\nEste é o Produto que deseja alterar? 1.SIM  2.NÃO")
+
+                escreva("\nOpção desejada: ")
+
+                leia(sn)
+
+                limpa()
+
+                se(sn == 1)
+
+                {
+                        escreva("Insira o novo código do fabricante: ")
+
+                    leia(cdFabricante[i])
+
+                    cdFabricante[i] = tx.caixa_alta(cdFabricante[i])
+
+                    escreva("Digite nome do Produto: ")
+
+                    leia(nmProduto[i])
+
+                    nmProduto[i] = tx.caixa_alta(nmProduto[i])
+
+                    escreva("Insira a nova cor: ")
+
+                    leia(corProduto[i])
+
+                    corProduto[i] = tx.caixa_alta(corProduto[i])
+
+                    //Exibe o Novo Produto
+                        escreva("\nCódigo: ", cdProduto[i])
+
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                        espaco = ""
+
+                    para(inteiro c = tx.numero_caracteres(guardarNome); c < 20; c++)
+					{
+                            espaco = espaco + " "
+
+                    }
+
+                        // Exibe
+                        escreva("\nAntes-->  Fabric.: ", guardarCodFab, " | Prod.: ", guardarNome, espaco, " | Cor: ", guardarCor)
+
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                        espaco = ""
+
+                    para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+					{
+                            espaco = espaco + " "
+
+                    }
+
+                        // Exibe
+                        escreva("\nDepois--> Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i])
+
+                    //Confirmação do Usuário
+                        escreva("\nConfirma a alteração? 1.SIM 2.NÃO ")
+
+                    escreva("\nOpção desejada: ")
+
+                    leia(sn2)
+
+                    se(sn2 == 1)
+
+                    {
+                            escreva("Produto Alterado com Sucesso!\n-----------------------------------------------------------------\n")
+
+                        escreva("Pressione Enter para retornar ao menu...")
+
+                        leia(nada)
+
+                        pare
+
+                    }
+
+                        //retorna os valores iniciais do vetor selecionado
+                        senao
+
+                    {
+                            cdProduto[i] = guardarCod
+
+                        nmProduto[i] = guardarNome
+
+                        cdFabricante[i] = guardarCodFab
+
+                        corProduto[i] = guardarCor
+
+                        escreva("\nProduto não alterado")
+
+                        pare
+
+                    }
+                    }
+                    senao
+
+                {
+                        escreva("\nProduto não alterado!")
+
+                    leia(nada)
+
+                    pare
+
+                }
+                }
+                //Vacilou
+                se(i == 998)
+
+            {
+                    escreva("Produto não existe, favor verificar a lista.")
+
+                escreva("\nPressione Enter para retornar ao menu... ")
+
+                leia(nada)
+
+                pare
+
+            }
+            }
+        }
+
+        static void ExcluirProduto(cadeia &cdProduto[], cadeia &nmProduto[], cadeia &cdFabricante[], cadeia &corProduto[])
+        {
+            // FUNÇÃO muda o valor do item buscado "zzz" o excluindo
+            cadeia excluido, nada, auxCad, espaco
+    
+        inteiro i, sn, x, y
+    
+
+        escreva("Digite o código do produto que deseja excluir: ")
+    
+        leia(excluido)
+    
+        excluido = tx.preencher_a_esquerda('0', 3, excluido)
+    
+        limpa()
+            //laço para encontrar o vetor código
+            para(i = 0; i < 999; i++)
+		{
+                se(cdProduto[i] == excluido)
+
+            {
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                    espaco = ""
+
+                para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+				{
+                        espaco = espaco + " "
+
+                }
+
+                    // Exibe
+                    escreva("O produto à seguir será excluído:\n")
+
+                escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i])
+
+                //Chance para o Usuário desistir
+                    escreva("\n\nConfirma a exclusão? 1.SIM 2.NÃO  ")
+
+                escreva("\nOpção desejada: ")
+
+                leia(sn)
+
+                se(sn == 1)
+
+                {
+                        nmProduto[i] = cdFabricante[i] = corProduto[i] = "zzz"
+
+                    escreva("\nO produto foi excluido")
+
+                }
+                    senao
+
+                {
+                        escreva("\nO produto não foi excluido")
+
+                }
+                    escreva("\nPressione Enter para retornar ao menu...")
+
+                leia(nada)
+
+                pare
+
+
+            }
+                //Vacilou
+                se(i == 998)
+
+            {
+                    escreva("Produto não existe, favor verificar a lista.")
+
+                escreva("Pressione Enter para retornar ao menu...")
+
+                leia(nada)
+
+                pare
+
+            }
+            }
+        }
+
+        static void MenuListagem(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            caracter listagem
+    
+        escreva("Qual tipo de lista deseja ver? \n")
+    
+
+        escreva("1. Produtos ordenados por nome (em ordem alfabetica de um ponto a outro)\n")
+    
+        escreva("2. Listagem dos produtos por código\n")
+    
+        escreva("3. Exibir produtos de determinado fabricante\n")
+    
+        escreva("4. Exibição dos produtos pertencentes a detemrinada cor\n")
+    
+        escreva("5. Listagem de todos os produtos em estoque com todas as informações existentes sobre cada um dos produtos\n")
+    
+
+        leia(listagem)
+    
+        limpa()
+    
+        escolha(listagem)
+    
+        {
+                caso '1': ListaNome(cdProduto, nmProduto, cdFabricante, corProduto) pare
+               caso '2': ListaCod(cdProduto, nmProduto, cdFabricante, corProduto) pare
+              caso '3': ListaFab(cdProduto, nmProduto, cdFabricante, corProduto) pare
+             caso '4': ListaCor(cdProduto, nmProduto, cdFabricante, corProduto) pare
+            caso '5': Lista(cdProduto, nmProduto, cdFabricante, corProduto) pare
+
+        }
+        }
+
+        static void ListaNome(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            cadeia auxCad, nada, nmInicial, nmFinal, espaco
+    
+        inteiro x, y, i, cont
+    
+        escreva("Qual a letra inicial: ")
+    
+        leia(nmInicial)
+    
+        nmInicial = tx.caixa_alta(nmInicial)
+    
+        escreva("Qual a letra final: ")
+    
+        leia(nmFinal)
+    
+        nmFinal = tx.caixa_alta(nmFinal)
+    
+        // Verifica se a letra inicial não é maior que a letra final
+            se(nmInicial > nmFinal)
+    
+        {
+                escreva("ERRO! A letra inicial maior que final.")
+
+        }
+            //Verifica se foram digitados números
+            senao se(tp.cadeia_e_inteiro(nmInicial, 10) == verdadeiro ou tp.cadeia_e_inteiro(nmFinal, 10) == verdadeiro)
+		{
+                escreva("Digite somente letras!")
+
+        }
+            senao
+    
+        {
+                // Reordena no método alfabético
+                para(x = 0; x < 998; x++)
+			{
+                    para(y = x + 1; y < 999; y++)
+	
+				se(nmProduto[x] > nmProduto[y])
+
+                {
+                        auxCad = nmProduto[x]
+
+                    nmProduto[x] = nmProduto[y]
+
+                    nmProduto[y] = auxCad
+
+
+                    auxCad = cdProduto[x]
+
+                    cdProduto[x] = cdProduto[y]
+
+                    cdProduto[y] = auxCad
+
+
+                    auxCad = cdFabricante[x]
+
+                    cdFabricante[x] = cdFabricante[y]
+
+                    cdFabricante[y] = auxCad
+
+
+                    auxCad = corProduto[x]
+
+                    corProduto[x] = corProduto[y]
+
+                    corProduto[y] = auxCad
+
+                }
+                }
+                limpa()
+
+            //Produtos em ordem alfabética no intervalo especificado
+                escreva("Todos os produtos de ", nmInicial, " até ", nmFinal, " em Ordem Alfabética \n\n")
+
+            cont = 0
+
+            nmFinal = nmFinal + "zzzzz"
+
+
+            para(i = 0; i < 999; i++)
+			{
+                    se(nmProduto[i] != "zzz" e nmInicial <= nmFinal e nmProduto[i] >= nmInicial e nmProduto[i] <= nmFinal)
+
+                {
+                        // formata o que será exibido conforme o tamanho da palavra do produto
+                        espaco = ""
+
+                    para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+					{
+                            espaco = espaco + " "
+
+                    }
+
+                        // Exibe
+                        cont++
+
+                    escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i], "\n")
+
+                }
+                }
+
+                //Validação dos dados inseridos
+                se(cont != 0)
+
+            {
+                    escreva("Encontrado(s) ", cont, " produtos")
+
+            }
+
+                se(i == 998 ou cont == 0)
+
+            {
+                    escreva("Não existem produtos nesta faixa: ")
+
+            }
+            }
+            escreva("\nPressione Enter para retornar ao menu... ")
+    
+        leia(nada)
+    
+    }
+
+        static void ListaCod(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            //declarando variáveis
+            cadeia auxCad, nada, cdInicial, cdFinal, espaco
+    
+        inteiro x, y, i, cont
+    
+        //Inicializar variáveis 
+            escreva("Qual o número inicial: ")
+    
+        leia(cdInicial)
+    
+        cdInicial = tx.preencher_a_esquerda('0', 3, cdInicial)
+    
+        escreva("Qual a número final: ")
+    
+        leia(cdFinal)
+    
+        cdFinal = tx.preencher_a_esquerda('0', 3, cdFinal)
+    
+
+        se(cdInicial > cdFinal)
+    
+        {
+                escreva("ERRO! O número inicial  é maior que o final.")
+
+        }
+            senao se(tp.cadeia_e_inteiro(cdInicial, 10) == falso ou tp.cadeia_e_inteiro(cdFinal, 10) == falso)
+		{
+                escreva("Digite somente números!")
+
+        }
+            senao
+    
+        {
+                //Rotina Ordena todos os vetores
+                para(x = 0; x < 998; x++)
+			{
+                    para(y = x + 1; y < 999; y++)
+	
+				se(cdProduto[x] > cdProduto[y])
+
+                {
+                        auxCad = nmProduto[x]
+
+                    nmProduto[x] = nmProduto[y]
+
+                    nmProduto[y] = auxCad
+
+
+                    auxCad = cdProduto[x]
+
+                    cdProduto[x] = cdProduto[y]
+
+                    cdProduto[y] = auxCad
+
+
+                    auxCad = cdFabricante[x]
+
+                    cdFabricante[x] = cdFabricante[y]
+
+                    cdFabricante[y] = auxCad
+
+
+                    auxCad = corProduto[x]
+
+                    corProduto[x] = corProduto[y]
+
+                    corProduto[y] = auxCad
+
+                }
+                }
+                limpa()
+
+            //Produtos em ordem alfabética no intervalo
+                escreva("Todos os produtos de ", cdInicial, " até ", cdFinal, " em Ordem Alfabética \n\n")
+
+            cdFinal = cdFinal + "zzzzz"
+
+            cont = 0
+
+
+            para(i = 0; i < 999; i++)
+			{
+                    se(cdProduto[i] != "zzz" e cdInicial <= cdFinal e cdProduto[i] >= cdInicial e cdProduto[i] <= cdFinal)
+
+                {
+                        // formata o que será exibido conforme o tamanho da palavra do produto
+                        espaco = ""
+
+                    para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+					{
+                            espaco = espaco + " "
+
+                    }
+
+                        // Exibe
+                        cont++
+
+                    escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i], "\n")
+
+                }
+                }
+
+                //Validação dos dados inseridos do USUÁRIO
+                se(cont != 0)
+
+            {
+                    escreva("Encontrado(s) ", cont, " produtos")
+
+            }
+
+                se(i == 998 ou cont == 0)
+
+            {
+                    escreva("\nNão existem produtos nesta faixa: ")
+
+            }
+            }
+            escreva("\nPressione Enter para retornar ao menu... ")
+    
+        leia(nada)
+    
+    }
+
+        static void ListaFab(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            cadeia auxCad, nada, fab, espaco
+    
+        inteiro i
+    
+
+        escreva("Qual Fabricante procura? ")
+    
+        leia(fab)
+    
+        fab = tx.caixa_alta(fab)
+    
+
+        limpa()
+    
+        para(i = 0; i < 999; i++)
+		{
+                se(cdFabricante[i] == fab)
+
+            {
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                    espaco = ""
+
+                para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+				{
+                        espaco = espaco + " "
+
+                }
+
+                    // Exibe
+                    escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i], "\n")
+
+            }
+            }
+            escreva("\nPressione Enter para retornar ao menu... ")
+    
+        leia(nada)
+    
+    }
+
+        static void ListaCor(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            cadeia auxCad, nada, cor, espaco
+    
+        inteiro i
+    
+
+        escreva("Qual Cor procura? ")
+    
+        leia(cor)
+    
+        cor = tx.caixa_alta(cor)
+    
+
+        limpa()
+    
+        para(i = 0; i < 999; i++)
+		{
+                se(corProduto[i] == cor)
+
+            {
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                    espaco = ""
+
+                para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+				{
+                        espaco = espaco + " "
+
+                }
+
+                    // Exibe
+                    escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i], "\n")
+
+            }
+            }
+            escreva("\nPressione Enter para retornar ao menu... ")
+    
+        leia(nada)
+    
+    }
+
+        static void Lista(cadeia cdProduto[], cadeia nmProduto[], cadeia cdFabricante[], cadeia corProduto[])
+        {
+            inteiro i, totaItens
+    
+        cadeia nada, espaco
+    
+        para(i = 0; i < 999; i++)
+		{
+                se(nmProduto[i] != "zzz")
+
+            {
+                    // formata o que será exibido conforme o tamanho da palavra do produto
+                    espaco = ""
+
+                para(inteiro c = tx.numero_caracteres(nmProduto[i]); c < 20; c++)
+				{
+                        espaco = espaco + " "
+
+                }
+
+                    // Exibe
+                    escreva("Cód.: ", cdProduto[i], " | Fabric.: ", cdFabricante[i], " | Prod.: ", nmProduto[i], espaco, " | Cor: ", corProduto[i], "\n")
+
+                totaItens = i
+
+            }
+            }
+            totaItens++
+    
+        escreva("\nTotal de " + totaItens + " itens na lista\n")
+    
+        escreva("Pressione Enter para retornar ao menu...")
+    
+        leia(nada)
+    
+    }
+
     }
 }
